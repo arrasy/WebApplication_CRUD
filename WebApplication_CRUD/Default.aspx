07b2a64df14c9c946c716713ac8e62d927006d80
Arrasy <arrasyjunior@gmail.com>
2021-01-05 10:12:52 +0700

    Add project files.

---
+++ WebApplication_CRUD/Default.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication_CRUD._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <br />
+    <div class="jumbotron" style="background-color: #8f0e22">
+        <div aria-disabled="False">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="Label4" runat="server" Font-Bold="True" Font-Size="XX-Large" ForeColor="#FFFFFF" Text=" User Registration Form"></asp:Label>
+        </div>
+            </div>
+    <asp:Label ID="LabelID" runat="server" Text="User ID"></asp:Label>
+&nbsp;
+<asp:TextBox ID="TextBoxID" runat="server"></asp:TextBox>
+    <br />
+    <br />
+        <asp:Label ID="LabelName" runat="server" Text="Name"></asp:Label>
+    &nbsp;&nbsp;&nbsp;
+<asp:TextBox ID="TextBoxName" runat="server"></asp:TextBox>
+    <br />
+    <br />
+    <p>
+        <asp:Label ID="LabelEmail" runat="server" Text="Email"></asp:Label>
+    &nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBoxEmail" runat="server"></asp:TextBox>
+    </p>
+      <br />
+<p>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="btInsert" runat="server" OnClick="btInsert_Click" Text="Insert" />
+&nbsp;&nbsp;
+        <asp:Button ID="btSearch" runat="server" Text="Search" OnClick="btSearch_Click" />
+&nbsp;&nbsp;
+        <asp:Button ID="btUpdate" runat="server" Text="Update" OnClick="btUpdate_Click" />
+&nbsp;&nbsp;
+        <asp:Button ID="btDelete" runat="server" Text="Delete" OnClick="btDelete_Click" onClientClick="return confirm('Are you sure to delete?');" />
+ <br />
+    <br />
+     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="lblmsgInsert" runat="server" Font-Bold="True" ForeColor="#990033" Text="Label"></asp:Label>
+     <br />
+     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:GridView ID="GridView1" runat="server" Width="412px">
+        </asp:GridView>
+</p>
+
+</asp:Content>
---
+++ WebApplication_CRUD/Default.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebApplication_CRUD.ServiceReference1;
+
+namespace WebApplication_CRUD
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
+        protected void btInsert_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=RASY;Initial Catalog=WCFServiceApp;Persist Security Info=True;User ID=sa;Password=bibimbap2326");
+            con.Open();
+            InsertUser u = new InsertUser();
+            u.Name = TextBoxName.Text;
+            u.Email = TextBoxEmail.Text;
+            string r = client.Insert(u);
+            lblmsgInsert.Text = r.ToString();
+            con.Close();
+            TextBoxID.Text = "";
+            TextBoxName.Text = "";
+            TextBoxEmail.Text = "";
+        }
+
+        protected void btSearch_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.GetTestData g = new ServiceReference1.GetTestData();
+            g = client.GetInfo();
+            DataTable dt = new DataTable();
+            dt = g.userTable;
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        protected void btUpdate_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=RASY;Initial Catalog=WCFServiceApp;Persist Security Info=True;User ID=sa;Password=bibimbap2326");
+            con.Open();
+            UpdateUser u = new UpdateUser();
+            u.UID = int.Parse(TextBoxID.Text);
+            u.Name = TextBoxName.Text;
+            u.Email = TextBoxEmail.Text;
+            string result = client.Update(u);
+            lblmsgInsert.Text = result.ToString();
+            con.Close();
+            TextBoxID.Text = "";
+            TextBoxName.Text = "";
+            TextBoxEmail.Text = "";
+        }
+
+        protected void btDelete_Click(object sender, EventArgs e)
+        {
+            GetTestData g = new GetTestData();
+            SqlConnection con = new SqlConnection("Data Source=RASY;Initial Catalog=WCFServiceApp;Persist Security Info=True;User ID=sa;Password=bibimbap2326");
+            con.Open();
+            DeleteUser d = new DeleteUser();
+            d.UID = int.Parse(TextBoxID.Text);
+            string res = client.Delete(d);
+            lblmsgInsert.Text = res.ToString();
+            con.Close();
+            TextBoxID.Text = "";
+            TextBoxName.Text = "";
+            TextBoxEmail.Text = "";
+        }
+    }
+}
